a1f5ed32d50af165c6a5a3aee93aa26e67a8c487
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-11 02:39:37 +0000

    Adding tarfile member sanitization to extractall()

--- py/jupyterlite/src/jupyterlite/addons/pyodide.py
+++ py/jupyterlite/src/jupyterlite/addons/pyodide.py
@@ -209,7 +209,29 @@ class PyodideAddon(BaseAddon):
         def _extract():
             with tempfile.TemporaryDirectory() as td:
                 with tarfile.open(local_path, "r:bz2") as zf:
-                    zf.extractall(td)
+                    
+                    import os
+                    
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(zf, td)
                 self.copy_one(Path(td), dest)
 
         task = dict(
